bde728ae920f923b6c8f521ebbb2012df5bb00a8
MichealDurex <anziloveanzi@Hotmail.com>
2013-10-11 17:29:08 +0800

    Signed-off-by: MichealDurex <anziloveanzi@Hotmail.com> 2013-10-11  Django: Config the database and config the admin page

--- DjangoWeb/urls.py
+++ DjangoWeb/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 #Index就是路由的名称，源自于什么文件
 from DjangoWeb.View.Index import index,detail,hours_ahead,template,home
 
+
+
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 	#元组中第一个元素是模式匹配字符串（正则表达式）；
@@ -15,4 +17,5 @@ urlpatterns = patterns('',
 	('^detail/plus/(\d{1,2})/$', hours_ahead),
 	('^template/$', template),
 	('^home/$', home),
+	('^admin/',include(admin.site.urls)),
 )
